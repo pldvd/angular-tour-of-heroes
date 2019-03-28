557ff7946987f296d287181ff67c5b0720d19a15
David Pal <paldavid2030@gmail.com>
2019-03-28 16:24:34 +0100

    inject messageService in message.component and bind its data to the view

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero'
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.addMessage('Your Heroes were fetched, my lord');
     return of(HEROES);
   }
 }
