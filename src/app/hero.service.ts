7fec0b6b847390a06e94db1bd0c544db23416845
David Pal <paldavid2030@gmail.com>
2019-04-01 13:36:44 +0200

    handle error: user types in a string as detail/:id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,7 +3,7 @@ import { HEROES } from './mock-heroes';
 import { Hero } from './hero'
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
-import { HeroesComponent } from './heroes/heroes.component';
+//import { HeroesComponent } from './heroes/heroes.component';
 
 @Injectable({
   providedIn: 'root'
@@ -17,19 +17,14 @@ export class HeroService {
     return of(HEROES);
   }
 
-  getHeroWithId(id: string): Observable<Hero> {
+  getHeroWithId(id: string): Observable<any> {
     const idNumber = Number(id);
 
-    try {
-      if (idNumber !== NaN) {
-        return of(HEROES.find(hero => hero.id === idNumber)
-        );
-      } else {
-        throw new Error('Id cannot be NaN');
-      }
-    } catch (error) {
-      console.log(error.message);
-      return null;
+    if (!Number.isNaN(idNumber)) {
+      console.log(idNumber);
+      return of(HEROES.find(hero => hero.id === idNumber));
+    } else {
+      return of(new Error('ID must be a Number'));;
     }
   }
 }
